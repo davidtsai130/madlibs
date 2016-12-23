be7b0c59269de15854923af8546e39aa2b076e67
davidtsai130 <davidtsai130@gmail.com>
2016-12-22 23:30:46 -0500

    basic stuff

---
+++ src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React, { PureComponent, PropTypes } from 'react'
+
+class Form extends PureComponent {
+  render() {
+    return(
+      <div>
+        Hai Rio
+      </div>
+    )
+  }
+}
+
+export default Form
